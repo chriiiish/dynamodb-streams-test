6b4db961ddd629158d5455787497bc9e9bb0023f
Chris Lloyd <chris.lloyd.hi@gmail.com>
2021-01-25 20:55:55 +1300

    Testing streams

--- lib/dynamo-copy-test-stack.ts
+++ lib/dynamo-copy-test-stack.ts
@@ -1,5 +1,9 @@
 import * as cdk from '@aws-cdk/core';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as sqs from '@aws-cdk/aws-sqs';
+import * as path from 'path';
+import * as eventsources from '@aws-cdk/aws-lambda-event-sources';
 
 export class DynamoCopyTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,7 +13,24 @@ export class DynamoCopyTestStack extends cdk.Stack {
     const table = new dynamodb.Table(this, 'original-table', {
       partitionKey: { name: 'customer', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'userid', type: dynamodb.AttributeType.STRING },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      stream: dynamodb.StreamViewType.NEW_IMAGE,
     });
+
+    const deadletter = new sqs.Queue(this, 'deadletter-queue');
+
+    const lambdafunction = new lambda.Function(this, 'processing-function', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+      handler: 'index.handler'
+    });
+    lambdafunction.addEventSource(new eventsources.DynamoEventSource(table, {
+      startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+      batchSize: 5,
+      bisectBatchOnError: true,
+      onFailure: new eventsources.SqsDlq(deadletter),
+      retryAttempts: 10
+    }))
+
   }
 }
